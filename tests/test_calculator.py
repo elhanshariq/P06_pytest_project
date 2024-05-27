2d01beb6bca6bc1da0ed530d682e472fe241d795
Elhan <23018539@myrp.edu.sg>
2024-05-27 14:11:50 +0800

    initial commit

---
+++ tests/test_calculator.py
@@ -0,0 +1,48 @@
+from calculator.calculator import Calculator
+
+class TestCalculator:
+    def test_add(self):
+        # arrange
+        a = 4321
+        b = 1234
+        cal = Calculator()
+
+        # act
+        result = cal.add(a, b)
+
+        # assert
+        expected = 5555
+        assert result == expected
+
+    def test_sub(self):
+        a=4321
+        b=1234
+
+        cal = Calculator()
+
+        result = cal.subtract(a,b)
+
+        expected = 3087
+        assert result == expected
+    
+    def test_multi(self):
+        a=4321
+        b=1234
+
+        cal = Calculator()
+
+        result = cal.multiply(a,b)
+
+        expected = 5332114
+        assert result == expected
+
+    def test_devi(self):
+        a=4
+        b=2
+
+        cal = Calculator()
+
+        result = cal.divide(a,b)
+
+        expected = 2
+        assert result == expected
